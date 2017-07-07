325d1fb86a342588a74b1198eff9cef205f337c0
Fahrazoid <anton.fakhr@mail.ru>
2017-07-07 12:28:50 +0300

    Basic configuration&markup, sass&jquery + portfolio slide

---
+++ js/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+  portflioSlide();
+  setSpecContent();
+});
+
+function portflioSlide() {
+
+  $('.img-unit').click(function() {
+    $('.portfolio-slide').css('left', '-100%');
+  });
+
+  $('.back-arrow').click(function() {
+    $('.portfolio-slide').css('left', '0');
+  });
+
+}
+
+function setSpecContent() {
+
+  $('.img-unit').click(function() {
+    let specTitle = $(this).find('p').text(),
+        specImage = $(this).css('background-image');
+
+        specImage = './' + specImage.slice(specImage.indexOf("assets"), -2);
+
+    $('.spec-title').text(specTitle);
+    $('.img-spec').attr('src', specImage);
+  });
+
+}
